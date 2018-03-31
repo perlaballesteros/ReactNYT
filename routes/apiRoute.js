69a2d08ef43530e83da99a193923ca4b7b1adbab
Perla Ballesteros <perlaballesteros@email.arizona.edu>
2018-03-31 13:57:23 -0700

    added the routes

---
+++ routes/apiRoute.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+const router = require("express").Router();
+
+router.get("/articles", (req, res) => {
+    console.log("he");
+  axios
+    .get("https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=5ebc6c97b9d54381b6f387bfc06b79dd&q=")
+    .then(({ data: { response} }) => res.json(response))
+    .catch(err => res.status(422).json(err));
+});
+
+module.exports = router;
